1bf7bdc6d09046deffa493a766a0a91926d3dfe0
nat <nathalia.n.reis@aluno.senai.br>
2026-03-16 19:31:41 -0300

    feat: diciona botão de baixar crachá e arruma estilização

--- App.js
+++ App.js
@@ -1,99 +1,119 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+} from 'react-native';
 import React from 'react';
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Dados do Aluno</Text>
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.titulo}>Dados do Aluno</Text>
 
-            <View style={styles.section}>
-                <View style={styles.div}>
-                    <View style={styles.campo}>
-                        <Text style={styles.cracha}>Nome</Text>
-                        <Text style={styles.exigencia}>*digite seu nome completo</Text>
+                <View style={styles.section}>
+                    <View style={styles.div}>
+                        <View style={styles.campo}>
+                            <Text style={styles.cracha}>Nome</Text>
+                            <Text style={styles.exigencia}>*digite seu nome completo</Text>
+                        </View>
+                        <TextInput
+                            style={styles.formulario}
+                            placeholder="Digite seu nome..."
+                            placeholderTextColor="#FFFFFF"></TextInput>
                     </View>
-                    <TextInput
-                        style={styles.formulario}
-                        placeholder="Digite seu nome..."
-                        placeholderTextColor="#FFFFFF"></TextInput>
+                    <View style={styles.div}>
+                        <View style={styles.campo}>
+                            <Text style={styles.cracha}>Email</Text>
+                        </View>
+                        <TextInput
+                            style={styles.formulario}
+                            placeholder="Digite seu email (exemplo: aluno@gmail.com)..."
+                            keyboardType="email-address"
+                            placeholderTextColor="#FFFFFF"></TextInput>
+                    </View>
+                    <View style={styles.div}>
+                        <View style={styles.campo}>
+                            <Text style={styles.cracha}>Número de matrícula</Text>
+                            <Text style={styles.exigencia}>*apenas números</Text>
+                        </View>
+                        <TextInput
+                            style={styles.formulario}
+                            placeholder="Digite sua matrícula..."
+                            keyboardType="num"
+                            placeholderTextColor="#FFFFFF"></TextInput>
+                    </View>
+                    <View style={styles.div}>
+                        <View style={styles.campo}>
+                            <Text style={styles.cracha}>Senha de acesso</Text>
+                        </View>
+                        <TextInput
+                            style={styles.formulario}
+                            placeholder="Digite sua senha..."
+                            keyboardType="visible-password"
+                            placeholderTextColor="#FFFFFF"></TextInput>
+                    </View>
                 </View>
-                <View style={styles.div}>
-                    <View style={styles.campo}>
-                        <Text style={styles.cracha}>Email</Text>
-                    </View>
-                    <TextInput
-                        style={styles.formulario}
-                        placeholder="Digite seu email (exemplo: aluno@gmail.com)..."
-                        keyboardType="email-address"
-                        placeholderTextColor="#FFFFFF"></TextInput>
+                <View style={styles.botaoSection}>
+                    <TouchableOpacity>
+                        <View style={styles.botao}>
+                            <Text style={styles.textoBotao}>Gerar crachá</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.div}>
-                    <View style={styles.campo}>
-                        <Text style={styles.cracha}>Número de matrícula</Text>
-                        <Text style={styles.exigencia}>*apenas números</Text>
-                    </View>
-                    <TextInput
-                        style={styles.formulario}
-                        placeholder="Digite sua matrícula..."
-                        keyboardType="num"
-                        placeholderTextColor="#FFFFFF"></TextInput>
-                </View>
-                <View style={styles.div}>
-                    <View style={styles.campo}>
-                        <Text style={styles.cracha}>Senha de acesso</Text>
-                    </View>
-                    <TextInput
-                        style={styles.formulario}
-                        placeholder="Digite sua senha..."
-                        keyboardType="visible-password"
-                        placeholderTextColor="#FFFFFF"></TextInput>
-                </View>
-            </View>
-            <View style={styles.botaoSection}>
-                <TouchableOpacity>
-                    <View style={styles.botao}>
-                        <Text style={styles.textoBotao}>Gerar crachá</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
 
-            <View style={styles.sectionCracha}>
-                <Text style={styles.subtitulo}>Crachá Digital</Text>
-                <View style={styles.conteinerCracha}>
-                    <View style={styles.info}>
-                        <View style={styles.textosInfo}>
-                            <Text style={styles.tituloInfo}>Nome:</Text>
-                            <Text style={styles.textInfo}>João Marcos da Silva</Text>
-                        </View>
-                        <View style={styles.textosInfo}>
-                            <Text style={styles.tituloInfo}>Email:</Text>
-                            <Text style={styles.textInfo}>joao.ms@aluno.senai,edu.br</Text>
-                        </View>
-                        <View style={styles.textosInfo}>
-                            <Text style={styles.tituloInfo}>Matrícula:</Text>
-                            <Text style={styles.textInfo}>123456789</Text>
+                <View style={styles.crachaEBotao}>
+                    <View style={styles.sectionCracha}>
+                        <Text style={styles.subtitulo}>Crachá Digital</Text>
+                        <View style={styles.conteinerCracha}>
+                            <View style={styles.info}>
+                                <View style={styles.textosInfo}>
+                                    <Text style={styles.tituloInfo}>Nome:</Text>
+                                    <Text style={styles.textInfo}>João Marcos da Silva</Text>
+                                </View>
+                                <View style={styles.textosInfo}>
+                                    <Text style={styles.tituloInfo}>Email:</Text>
+                                    <Text style={styles.textInfo}>joao.ms@aluno.senai,edu.br</Text>
+                                </View>
+                                <View style={styles.textosInfo}>
+                                    <Text style={styles.tituloInfo}>Matrícula:</Text>
+                                    <Text style={styles.textInfo}>123456789</Text>
+                                </View>
+                            </View>
+                            <Image
+                                source={require('./assets/qrcode.png')}
+                                style={{ width: 65, height: 65, alignSelf: 'center' }}></Image>
                         </View>
                     </View>
-                    <Image
-                        source={require('./assets/qrcode.png')}
-                        style={{ width: 65, height: 65, alignSelf: 'center' }}></Image>
+                    <View style={styles.botaoDiv}>
+                        <TouchableOpacity>
+                            <View style={styles.botao}>
+                                <Text style={styles.textoBotao}>Baixar crachá</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+                <Image></Image>
             </View>
-        </View>
+        </ScrollView>
     );
 };
 export default App;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'lavender',
+        backgroundColor: '#ffffff',
         flex: 1,
         alignItems: 'center',
     },
     titulo: {
         color: '#000000',
         fontFamily: 'Gloock-Regular',
-        fontSize: 50,
+        fontSize: 43,
         margin: 50,
     },
     section: {
@@ -116,7 +136,7 @@ const styles = StyleSheet.create({
     },
     cracha: {
         color: '#414141',
-        fontSize: 20,
+        fontSize: 18,
     },
     exigencia: {
         color: '#6F9F9E',
@@ -127,11 +147,11 @@ const styles = StyleSheet.create({
         width: 'auto',
         height: 35,
         borderRadius: 10,
-        padding: 5,
+        padding: 6,
     },
     botaoSection: {
         alignItems: 'center',
-        margin: 30,
+        marginTop: 30
     },
     botao: {
         width: 120,
@@ -144,6 +164,9 @@ const styles = StyleSheet.create({
         color: '#414141',
         alignSelf: 'center',
         fontSize: 14,
+    },
+    crachaEBotao: {
+        margin: 30,
     },
     sectionCracha: {
         width: 300,
@@ -171,10 +194,13 @@ const styles = StyleSheet.create({
     },
     textosInfo: {
         color: '#ffffff',
-
     },
     info: {
         gap: 10,
         color: '#FFFFFF',
+    },
+    botaoDiv: {
+        alignSelf: 'flex-start',
+        marginTop: 30
     },
 });
